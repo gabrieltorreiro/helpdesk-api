2b6b8e2623210d8bec67b81edb3d86c3c0653323
gabrieltorreiro <gabriellopesbusiness@gmail.com>
2026-04-21 19:45:15 -0300

    refactor: remove duplicated login

--- app/routes/user.py
+++ app/routes/user.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import Session
 
 from app.database.connection import SessionLocal
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.user import UserResponse
+from app.core.permissions import require_role
 
 router = APIRouter(prefix="/users", tags=["Users"])
-
-pwd_context = PasswordHash.recommended()
 
 
 def get_db():
@@ -19,32 +18,10 @@ def get_db():
         db.close()
 
 
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
-
-
-@router.post("/", response_model=UserResponse)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-
-    # Verifica se já existe
-    existing_user = db.query(User).filter(User.email == user.email).first()
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    new_user = User(
-        name=user.name,
-        email=user.email,
-        password=hash_password(user.password)
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-
-
 @router.get("/", response_model=list[UserResponse])
-def get_users(db: Session = Depends(get_db)):
+def get_users(
+    db: Session = Depends(get_db),
+    current_user = Depends(require_role(["admin"]))
+):
     users = db.query(User).all()
     return users
