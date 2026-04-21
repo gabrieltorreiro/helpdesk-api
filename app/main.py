dd43b37b0e44817805b0566f340d212cd6e82dc2
gabrieltorreiro <gabriellopesbusiness@gmail.com>
2026-04-21 19:59:24 -0300

    chore: Add a line to simplify table creation

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routes import auth, user, ticket, comment
 
 app = FastAPI(title="HelpDesk API")
 
+# Base.metadata.create_all(bind=engine) # Uncomment this line the first time to create all tables in your database.
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(ticket.router)
